9bc32277121a8b96bc058317d04c8a1af12638d1
vezenovm <mvezenov@gmail.com>
2023-02-25 01:07:54 +0000

    cleanup compile hasher script

--- scripts/compileHasher.ts
+++ scripts/compileHasher.ts
@@ -1,14 +1,7 @@
-// Generates Hasher artifact at compile-time using Truffle's external compiler
-// mechanism
-// const path = require('path')
-// const fs = require('fs')
-// const genContract = require('circomlib/src/mimcsponge_gencontract.js')
-
 import path from 'path';
 import { writeFileSync } from 'fs';
 // @ts-ignore -- no types
 import { mimc7Contract as mimcContract } from 'circomlibjs';
-// import { mimcSpongecontract as mimcContract } from 'circomlibjs';
 
 // where Truffle will expect to find the results of the external compiler
 // command
